56316477bec40cde0d8d85b17e9abf9068f132e2
Agata <agata.weltrowska@outlook.com>
2019-07-17 17:52:12 +0200

    stary commit

---
+++ src/main/java/com/javagda25/CalculatedRates.java
@@ -0,0 +1,8 @@
+package com.javagda25;
+
+import lombok.Data;
+
+@Data
+public class CalculatedRates {
+    private Double ask, mid, bid;
+}
--- src/main/java/com/javagda25/model/Rate.java
+++ src/main/java/com/javagda25/model/Rate.java
@@ -1,8 +1,15 @@
 package com.javagda25.model;
 
+import lombok.*;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @XmlRootElement(name="Rate")
 public class Rate {
     @XmlElement(name = "No")
